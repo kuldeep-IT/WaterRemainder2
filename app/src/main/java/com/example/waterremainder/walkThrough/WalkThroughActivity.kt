4c6583fe059520822490d9eea56155a4907ea25a
Ami Trambadia <ami@peerbits.com>
2022-10-13 18:22:22 +0530

    Initial commit

---
+++ app/src/main/java/com/example/waterremainder/walkThrough/WalkThroughActivity.kt
@@ -0,0 +1,12 @@
+package com.example.waterremainder.walkThrough
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.waterremainder.R
+
+class WalkThroughActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_walk_through)
+    }
+}
