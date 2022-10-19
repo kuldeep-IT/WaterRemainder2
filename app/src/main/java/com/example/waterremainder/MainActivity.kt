b7b81b9ed89a0f02233e60b8493e8c0948b09b23
kuldeep-IT <rathod99244@gmail.com>
2022-10-19 12:31:09 +0530

    - tabBar Layout - viewPager2 - tabAdapter

--- app/src/main/java/com/example/waterremainder/MainActivity.kt
+++ app/src/main/java/com/example/waterremainder/MainActivity.kt
@@ -3,13 +3,23 @@ package com.example.waterremainder
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
+import android.os.Build
 import android.os.Bundle
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.asLiveData
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.waterremainder.adapter.TabPagerAdapter
+import com.example.waterremainder.databinding.ActivityMainBinding
+import com.example.waterremainder.fragments.HistoryFragment
+import com.example.waterremainder.fragments.HomeFragment
+import com.example.waterremainder.fragments.SettingsFragment
 import com.example.waterremainder.utils.DataStoreManager
 import com.example.waterremainder.utils.Keys.FIRST_RUN
 import com.example.waterremainder.utils.PreferenceKeys
 import com.example.waterremainder.walkThrough.MainWalkThrough
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,20 +29,53 @@ class MainActivity : AppCompatActivity() {
     lateinit var dataStoreManager: DataStoreManager
     var isFirstRun: Boolean = false
 
+    lateinit var adapter: TabPagerAdapter
+    lateinit var binding: ActivityMainBinding
+
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
 
 
         sharedPreferences = getPreferences(Context.MODE_PRIVATE)
         sharedEditor = sharedPreferences.edit()
-        /* dataStoreManager = rember(this)  { DataStoreManager(this) }*/
         dataStoreManager = DataStoreManager(applicationContext)
 
         if (isItFirstRun()) {
             startActivity(Intent(this@MainActivity, MainWalkThrough::class.java))
             finish()
         }
+
+        setTabPagerAdapter()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    fun setTabPagerAdapter(){
+        adapter = TabPagerAdapter(this@MainActivity)
+
+        adapter.addFragment(HomeFragment.newInstance().apply {
+
+        }, "Home")
+
+        adapter.addFragment(HistoryFragment.newInstance().apply {
+
+        }, "History")
+
+        adapter.addFragment(SettingsFragment.newInstance().apply {
+
+        }, "Settings")
+
+        binding.viewPager2.adapter = adapter
+        binding.viewPager2.currentItem = 0
+        TabLayoutMediator(binding.tabs, binding.viewPager2){ tab, position ->
+            tab.text = adapter.getTabTitle(position)
+        }.attach()
+
+        binding.tabs.setSelectedTabIndicatorColor(getColor(R.color.white))
+
     }
 
     fun isItFirstRun(): Boolean {
---
+++ app/src/main/java/com/example/waterremainder/adapter/TabPagerAdapter.kt
@@ -0,0 +1,30 @@
+package com.example.waterremainder.adapter
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class TabPagerAdapter(activity: FragmentActivity?) : FragmentStateAdapter(activity!!) {
+
+    private val mFragmentList: MutableList<Fragment> = ArrayList()
+    private val mFragmentTitleList: MutableList<String> = ArrayList()
+
+    fun getTabTitle(position : Int): String{
+        return mFragmentTitleList[position]
+    }
+
+    fun addFragment(fragment: Fragment, title: String) {
+        mFragmentList.add(fragment)
+        mFragmentTitleList.add(title)
+    }
+
+    override fun getItemCount(): Int {
+        return mFragmentList.size
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return mFragmentList[position]
+    }
+}
